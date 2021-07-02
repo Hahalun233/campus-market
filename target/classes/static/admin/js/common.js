17a303770dfd310d1573ae07b5588c0da383fa23
Hahalun233 <757988842@qq.com>
2021-07-02 21:47:56 +0800

    2021/7/2

--- target/classes/static/admin/js/common.js
+++ target/classes/static/admin/js/common.js
@@ -1,3 +1,10 @@
+window.alert =alert;
+function alert(e){
+    $("body").append('<div id="msg"><div id="msg_top"><span class="msg_close">×</span></div><div id="msg_cont">'+e+'</div><div class="msg_close" id="msg_clear">确定</div></div>');
+    $(".msg_close").click(function (){
+        $("#msg").remove();
+    });
+}
 //显示成功信息
 function showSuccessMsg(msg,callback){
 	$.confirm({
